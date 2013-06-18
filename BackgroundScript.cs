eaeaebe01febd425c9d50cf08c7454857603d3ae
jihwankim1212 <whvhrclub@naver.com>
2013-06-18 15:29:17 +0900

    주석 처리

--- BackgroundScript.cs
+++ BackgroundScript.cs
@@ -12,7 +12,7 @@ public class BackgroundScript : MonoBehaviour {
 	//강이 흘러가는 속도
         float offset = Time.time * scrollSpeed;
 		
-		//Set the texture offset
+		//Set the texture offset //material에 속도 offset을 부여함
         renderer.material.mainTextureOffset = new Vector2(0,-offset);
     }
 }
